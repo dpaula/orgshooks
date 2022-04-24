921ecda0b3e220f92392694a1a0301a182dbc716
Fernando de Lima <brmou0146@ambev.com.br>
2022-04-24 11:02:11 -0300

    utilizando FlatList para renderizar a lista na tela

--- src/telas/componentes/Produtores.js
+++ src/telas/componentes/Produtores.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Text } from 'react-native';
+import { Text, FlatList } from 'react-native';
 
 import { carregaProdutores } from '../../services/carregaDados';
 
@@ -9,15 +9,23 @@ export default function Produtores() {
   //o primeiro elemento é o valor inicial do estado, o segundo é uma função para atualizar o valor do estado
   //sempre deve ser o primeiro a ser declarado na função
   const [titulo, setTitulo] = useState('');
+  const [lista, setLista] = useState([]);
 
   //useEffect é um método que executa uma função após o componente ser montado
   useEffect(() => {
     const retorno = carregaProdutores();
     setTitulo(retorno.titulo);
+    setLista(retorno.lista);
 
   }, []);
 
   return (
-    <Text>{titulo}</Text>
+    <FlatList
+      //data é um array de objetos que será exibido na tela
+      data={lista}
+      //renderItem é uma funcao que recebe qual atributo será exibido da lista
+      renderItem={({ item: { nome } }) => <Text>{nome}</Text>}
+      //ListHeaderComponent é uma função que será exibida no topo da lista
+      ListHeaderComponent={() => <Text>{titulo}</Text>} />
   );
 }
