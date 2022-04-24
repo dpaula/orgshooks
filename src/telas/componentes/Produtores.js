773b6c730f7400e6c555211c6d1a3723cc5a7db9
Fernando de Lima <brmou0146@ambev.com.br>
2022-04-24 10:51:21 -0300

    usando hooks para defenir o estado useState dos dados carregados ao montar componente

--- src/telas/componentes/Produtores.js
+++ src/telas/componentes/Produtores.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Text } from 'react-native';
 
 import { carregaProdutores } from '../../services/carregaDados';
 
 export default function Produtores() {
+  //useState retorna um array com duas posições: 
+  //o primeiro elemento é o valor inicial do estado, o segundo é uma função para atualizar o valor do estado
+  //sempre deve ser o primeiro a ser declarado na função
+  const [titulo, setTitulo] = useState('');
 
   //useEffect é um método que executa uma função após o componente ser montado
   useEffect(() => {
     const retorno = carregaProdutores();
-    console.log(retorno);
+    setTitulo(retorno.titulo);
 
   }, []);
 
   return (
-    <Text>Produtores</Text>
+    <Text>{titulo}</Text>
   );
 }
