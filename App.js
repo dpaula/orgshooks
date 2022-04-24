961a8e707f8071bf26ce28c9c61c335901019439
Fernando de Lima <brmou0146@ambev.com.br>
2022-04-24 11:27:27 -0300

    aplicando style flex 1, para expandir toda tela do aparelho para view

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Home from './src/telas/index';
 
 function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={estilos.tela}>
       <Home />
     </SafeAreaView>
   );
 }
+
+const estilos = StyleSheet.create({
+  tela: {
+    //flex 1 é definido para que o conteudo da tela ocupe todo o espaço disponivel
+    flex: 1,
+  }
+});
 
 export default App;
